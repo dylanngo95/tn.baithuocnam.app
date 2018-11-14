b4ce8044aef5326b94ce5999a583293184c0ecb8
Jundat95 <it.tinhngo@gmail.com>
2018-11-14 08:18:23 +0700

    add NetworkInfo

--- src/features/start/start.component.tsx
+++ src/features/start/start.component.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { TabbarCustom } from '../../navigations/tabbar';
 import { RootStackGlobal } from '../../common/global/rootstack.global';
+import { NetInfo } from 'react-native';
+
 
 export interface StartProps {
     navigation: any;
@@ -13,6 +15,22 @@ export default class StartComponent extends React.Component<StartProps, StartSta
   constructor(props: StartProps) {
     super(props);
     RootStackGlobal.set(this.props.navigation);
+
+    NetInfo.isConnected.fetch().then(isConnected => {
+      console.warn('First, is ' + (isConnected ? 'online' : 'offline'));
+    });
+    function handleFirstConnectivityChange(isConnected: any) {
+      console.warn('Then, is ' + (isConnected ? 'online' : 'offline'));
+      // NetInfo.isConnected.removeEventListener(
+      //   'connectionChange',
+      //   handleFirstConnectivityChange
+      // );
+    }
+    NetInfo.isConnected.addEventListener(
+      'connectionChange',
+      handleFirstConnectivityChange
+    );
+
   }
 
   public render() {
